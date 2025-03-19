7abf7413c911996bd327596f0ef64fefca6b2fb8
Oscar <fernandoscar04@gmail.com>
2025-03-19 08:08:03 -0600

    Create DepartamentosViewModel

---
+++ PRUEBAAPI/Ferreteria/Ferreteria/Models/DepartamentosViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Ferreteria.Models
+{
+    public class DepartamentosViewModel
+    {
+        public string Depa_Codigo { get; set; }
+
+        public string Depa_Descripcion { get; set; }
+
+        public int Usua_Creacion { get; set; }
+
+        public DateTime Feca_Creacion { get; set; }
+
+        public int? Usua_Modificacion { get; set; }
+
+        public DateTime? Feca_Modificacion { get; set; }
+    }
+}
