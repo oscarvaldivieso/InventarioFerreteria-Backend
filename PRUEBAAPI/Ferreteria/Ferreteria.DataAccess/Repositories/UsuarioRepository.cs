4f02f363b43c524f21170da213febe194c680156
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-03-20 21:48:23 -0600

    Update UsuarioRepository

--- PRUEBAAPI/Ferreteria/Ferreteria.DataAccess/Repositories/UsuarioRepository.cs
+++ PRUEBAAPI/Ferreteria/Ferreteria.DataAccess/Repositories/UsuarioRepository.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ferreteria.DataAccess.Repositories
 {
-    class UsuarioRepository
+    public class UsuarioRepository
     {
+        private readonly string _connectionString;
+
+        public UsuarioRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<UsuarioLoginResponse> IniciarSesion(string usuario, string contrasena)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var parametros = new
+                {
+                    usuario,
+                    contrasena
+                };
+
+                return await connection.QueryFirstOrDefaultAsync<UsuarioLoginResponse>(
+                    "Acce.SP_Usuarios_InicioSesion",
+                    parametros,
+                    commandType: CommandType.StoredProcedure
+                );
+            }
+        }
     }
 }
